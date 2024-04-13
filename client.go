6b361b43bd43316c215deb362753027ff3111fc3
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-04-13 10:03:25 +0800

    Update client.go

--- client.go
+++ client.go
@@ -1,31 +1,68 @@
 package main
 
 import (
-	"encoding/binary"
+	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
-	"net"
+	"github.com/parnurzeal/gorequest"
+	"io"
+	"net/http"
 	"os"
 	"time"
-
-	"github.com/parnurzeal/gorequest"
 )
 
-type Request struct {
-	Token string `json:"token"`
-	URL   string `json:"url"`
+type CrawlerTask struct {
+	Token       string `json:"token"`        // 校验密码
+	Tag         string `json:"tag"`          // 商家TAG
+	URL         string `json:"url"`          // 需要爬取的链接
+	BillingType string `json:"billing_type"` // 爬取的类型
+	CrawlNum    int    `json:"crawl_num"`    // 包含的商品个数
+	ExtraHeader string `json:"extra_header"` // 额外的请求头
+	ReqMethod   string `json:"req_method"`   // 请求模式
 }
 
-type Response struct {
-	Token     string `json:"token"`
-	Runtime   int    `json:"runtime"`
-	StartTime string `json:"start_time"`
-	Success   bool   `json:"success"`
-	WebData   string `json:"webdata,omitempty"`
+type CrawlerResult struct {
+	Token       string `json:"token"`             // 校验密码
+	Tag         string `json:"tag"`               // 商家TAG
+	URL         string `json:"url"`               // 需要爬取的链接
+	BillingType string `json:"billing_type"`      // 爬取的类型
+	CrawlNum    int    `json:"crawl_num"`         // 包含的商品个数
+	Runtime     int    `json:"runtime"`           // 爬虫耗时
+	StartTime   string `json:"start_time"`        // 爬虫开始时间
+	Success     bool   `json:"success"`           // 是否成功抓取页面
+	ReqMethod   string `json:"req_method"`        // 请求模式
+	WebData     string `json:"webdata,omitempty"` // 页面的html源码
 }
 
 var spiderToken, dashboardHost, dashboardPort string
+
+func getOneTask() (CrawlerTask, error) {
+	url := "http://" + dashboardHost + ":" + dashboardPort + "/spiders/getonetask"
+	data := map[string]string{"token": spiderToken}
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return CrawlerTask{}, err
+	}
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return CrawlerTask{}, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return CrawlerTask{}, fmt.Errorf("HTTP request failed with status code: %d", resp.StatusCode)
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return CrawlerTask{}, err
+	}
+	var task CrawlerTask
+	err = json.Unmarshal(body, &task)
+	if err != nil {
+		return CrawlerTask{}, err
+	}
+	return task, nil
+}
 
 func main() {
 	flag.StringVar(&spiderToken, "token", "", "爬虫校验的Token")
@@ -38,83 +75,15 @@ func main() {
 		os.Exit(1)
 	}
 	for {
-		conn, err := net.Dial("tcp", dashboardHost+":"+dashboardPort)
+		task, err := getOneTask()
 		if err != nil {
-			fmt.Println("Error connecting:", err.Error())
+			fmt.Println("Error getting task:", err.Error())
 			time.Sleep(6 * time.Second)
 			continue
 		}
-		handleConnection(conn)
+		go handleTask(task)
+		time.Sleep(3 * time.Second)
 	}
-}
-
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-	for {
-		// 读取消息长度
-		lenBuf := make([]byte, 4)
-		_, err := conn.Read(lenBuf)
-		if err != nil {
-			fmt.Println("Error reading message length:", err.Error())
-			return
-		}
-		msgLen := binary.BigEndian.Uint32(lenBuf)
-		// 读取消息内容
-		buffer := make([]byte, msgLen)
-		n, err := conn.Read(buffer)
-		if err != nil {
-			fmt.Println("Error reading message:", err.Error())
-			return
-		}
-		var request Request
-		err = json.Unmarshal(buffer[:n], &request)
-		if err != nil {
-			fmt.Println("Error decoding request:", err.Error())
-			continue
-		}
-		go handleTask(conn, request)
-	}
-}
-
-func handleTask(conn net.Conn, request Request) {
-	if request.Token != spiderToken {
-		fmt.Println("Invalid token received. Ignoring the task.")
-		return
-	}
-	startTime := time.Now()
-	webData, success := fetchWebData(request.URL)
-	runtime := int(time.Since(startTime).Seconds())
-	loc, _ := time.LoadLocation("Asia/Shanghai")
-	beijingTime := time.Now().In(loc)
-	formattedTime := beijingTime.Format("2006-01-02 15:04:05")
-	response := Response{
-		Token:     request.Token,
-		Runtime:   runtime,
-		Success:   success,
-		StartTime: formattedTime,
-		WebData:   webData,
-	}
-	responseJSON, err := json.Marshal(response)
-	if err != nil {
-		fmt.Println("Error encoding response:", err.Error())
-		return
-	}
-	// 发送消息长度
-	msgLen := uint32(len(responseJSON))
-	lenBuf := make([]byte, 4)
-	binary.BigEndian.PutUint32(lenBuf, msgLen)
-	_, err = conn.Write(lenBuf)
-	if err != nil {
-		fmt.Println("Error sending message length:", err.Error())
-		return
-	}
-	// 发送消息内容
-	_, err = conn.Write(responseJSON)
-	if err != nil {
-		fmt.Println("Error sending response:", err.Error())
-		return
-	}
-	fmt.Println("Sent response size:", len(responseJSON))
 }
 
 func fetchWebData(url string) (string, bool) {
@@ -125,8 +94,60 @@ func fetchWebData(url string) (string, bool) {
 		fmt.Printf("Error reading response body: %v \n", url)
 		return "", false
 	}
-	fmt.Println("URL:", resp.Request.URL)
+	fmt.Println("URL:", resp.CrawlerTask.URL)
 	elapsedTime := time.Since(startTime)
 	fmt.Println("Time taken:", elapsedTime)
 	return body, true
+}
+
+func handleTask(task CrawlerTask) {
+	if task.Token != spiderToken {
+		fmt.Println("Invalid token received. Ignoring the task.")
+		return
+	}
+	if task.URL == "" || task.Tag == "" {
+		fmt.Println("Invalid URL or Tag. Ignoring the task.")
+		return
+	}
+	startTime := time.Now()
+	webData, success := fetchWebData(task.URL)
+	runtime := int(time.Since(startTime).Seconds())
+	loc, _ := time.LoadLocation("Asia/Shanghai")
+	beijingTime := time.Now().In(loc)
+	formattedTime := beijingTime.Format("2006-01-02 15:04:05")
+	response := CrawlerResult{
+		Token:       spiderToken,
+		Tag:         task.Tag,
+		CrawlNum:    task.CrawlNum,
+		BillingType: task.BillingType,
+		URL:         task.URL,
+		Runtime:     runtime,
+		Success:     success,
+		StartTime:   formattedTime,
+		ReqMethod:   task.ReqMethod,
+		WebData:     webData,
+	}
+	responseJSON, err := json.Marshal(response)
+	if err != nil {
+		fmt.Println("Error encoding response:", err.Error())
+		return
+	}
+	// 发送信息
+	url := "http://" + dashboardHost + ":" + dashboardPort + "/spiders/handletask"
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(responseJSON))
+	if err != nil {
+		fmt.Println("Error sending post:", err.Error())
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("HTTP request failed with status code: %d", resp.StatusCode)
+		return
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Error readall body: %v", err.Error())
+		return
+	}
+	fmt.Println("Sent response result:", string(body))
 }
