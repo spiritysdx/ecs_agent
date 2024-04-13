42013e3d469c0b8ac82f6b28de81a66dd7f094f1
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-04-13 15:11:29 +0800

    Update client.go

--- client.go
+++ client.go
@@ -93,7 +93,12 @@ func main() {
 func fetchWebData(url string) (string, bool) {
 	startTime := time.Now()
 	request := gorequest.New()
-	_, body, err := request.Get(url).End()
+	req := request.Get(url)
+	req.Set("User-Agent",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36")
+	req.Set("Accept",
+		"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+	_, body, err := req.End()
 	if err != nil {
 		fmt.Printf("Error reading response body: %v \n", url)
 		return "", false
